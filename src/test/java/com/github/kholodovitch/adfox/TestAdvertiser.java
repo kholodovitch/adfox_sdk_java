1fda8c8248d72d0a260fcc793d2891338fa03f3a
Alexander Kholodovitch <kholodovitch@gmail.com>
2015-11-16 14:33:01 +0300

    Generate random name of advertiser.

--- src/test/java/com/github/kholodovitch/adfox/TestAdvertiser.java
+++ src/test/java/com/github/kholodovitch/adfox/TestAdvertiser.java
@@ -2,6 +2,8 @@ package com.github.kholodovitch.adfox;
 
 import static org.junit.Assert.assertTrue;
 
+import java.util.UUID;
+
 import org.junit.Test;
 
 public class TestAdvertiser extends BaseTest {
@@ -13,7 +15,7 @@ public class TestAdvertiser extends BaseTest {
 	@Test
 	public void testAddAdvertiser() throws Exception {
 		Advertiser advertiser = new Advertiser();
-		advertiser.setAccount("XXX_0");
+		advertiser.setAccount("XXX_" + UUID.randomUUID().toString());
 		advertiser.setEMail("mail@example.com");
 
 		int advertiserId = client.account().add().advertiser(advertiser, "cb92d111bf8d6bcf59e905d5fc50d5d403f0a59bf9a857b711020ba083b524a9");
