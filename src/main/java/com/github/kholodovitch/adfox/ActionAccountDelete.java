06a35fef24f6655919ec92fe8e70eabba9555a1a
Alexander Kholodovitch <kholodovitch@gmail.com>
2015-11-25 18:55:18 +0300

    Fix deleting banner.

--- src/main/java/com/github/kholodovitch/adfox/ActionAccountDelete.java
+++ src/main/java/com/github/kholodovitch/adfox/ActionAccountDelete.java
@@ -1,7 +1,5 @@
 package com.github.kholodovitch.adfox;
 
-import org.apache.commons.lang3.StringUtils;
-
 import com.github.kholodovitch.adfox.exceptions.AdFoxException;
 import com.github.kholodovitch.adfox.interfaces.IActionAccountDelete;
 
@@ -14,7 +12,7 @@ public class ActionAccountDelete implements IActionAccountDelete {
 	}
 
 	public boolean banner(int campaignId, int... bannerIdArray) throws AdFoxException {
-		if (apiClient.deleteItem("account", "banner", "campaignID=" + campaignId, "listIDs=" + StringUtils.join(bannerIdArray, ",")) > 0)
+		if (apiClient.deleteItem("account", "banner", "campaignID=" + campaignId, "listIDs=" + concatStringsWSep(bannerIdArray, ",")))
 			return true;
 		else
 			return false;
@@ -35,4 +33,13 @@ public class ActionAccountDelete implements IActionAccountDelete {
 		return false;
 	}
 
+	public static String concatStringsWSep(int[] strings, String separator) {
+		StringBuilder sb = new StringBuilder();
+		String sep = "";
+		for (int s : strings) {
+			sb.append(sep).append(s);
+			sep = separator;
+		}
+		return sb.toString();
+	}
 }
